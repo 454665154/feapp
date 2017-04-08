2421cd9429fafd694dcfa96b259aac4296ec342e
cyan wang <454665154@qq.com>
2017-04-08 17:01:07 +0800

    remove all

--- feapp/utils/config/db-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    localDBConnnect: 'mongodb://localhost/feapp-local',
-    testDBConnnect: 'mongodb://10.58.57.5/feapp-test',
-    prdDBConnnect: 'mongodb://10.58.57.5/feapp-prd'
-}
--- feapp/utils/config/system-config-dev.js
+++
@@ -1,7 +0,0 @@
-/**
- * dev环境系统配置
- */
-module.exports = {
-    //构建项目根路径
-    buildProjectRootDir: "E:/myBuild/gfe/feapp",
-}
